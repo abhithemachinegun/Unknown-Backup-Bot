6ae7a25929ce2b1819307013cd91a830e398b39d
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2024-03-21 19:58:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhithemachinegun/-Unknown_cinema_bot.git /-Unknown_cinema_bot 
+  git clone https://github.com/abhithemachinegun/Unknown-Backup-Bot.git /Unknown-Backup-Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /-Unknown_cinema_bot 
+  git clone $UPSTREAM_REPO /Unknown-Backup-Bot 
 fi
 cd /Auto-filter 
 pip3 install -U -r requirements.txt
